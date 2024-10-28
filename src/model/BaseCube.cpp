6391943662aad826b4f0c1e72e5a1f65b488bb36
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2024-10-28 23:40:20 +0530

    added implementation of executeMove and executeInversionOfMove
    Signed-off-by: vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>

--- src/model/BaseCube.cpp
+++ src/model/BaseCube.cpp
@@ -6,11 +6,9 @@
 #include <string>
 #include <unordered_map>
 
-/*
- * Get the string representation of a move
- */
+
 std::string BaseCube::getMove(const Move move){
-    const std::unordered_map<Move,std::string> mp = {
+    static const std::unordered_map<Move,std::string> mp = {
         {Move::U, "U"}, {Move::U_PRIME, "U'"},
         {Move::D, "D"}, {Move::D_PRIME, "D'"},
         {Move::F, "F"}, {Move::F_PRIME, "F'"},
@@ -19,4 +17,30 @@ std::string BaseCube::getMove(const Move move){
         {Move::R, "R"}, {Move::R_PRIME, "R'"}
     };
     return mp.at(move);
+}
+
+BaseCube& BaseCube::executeMove(const Move move) {
+    static const std::unordered_map<Move, BaseCube& (BaseCube::*)()> mp = {
+        {Move::L, &BaseCube::l}, {Move::L_PRIME, &BaseCube::lPrime},
+        {Move::R, &BaseCube::r}, {Move::R_PRIME, &BaseCube::rPrime},
+        {Move::U, &BaseCube::u}, {Move::U_PRIME, &BaseCube::uPrime},
+        {Move::D, &BaseCube::d}, {Move::D_PRIME, &BaseCube::dPrime},
+        {Move::F, &BaseCube::f}, {Move::F_PRIME, &BaseCube::fPrime},
+        {Move::B, &BaseCube::b}, {Move::B_PRIME, &BaseCube::bPrime}
+    };
+    const auto it = mp.find(move);
+    return (this->*it->second)();
+}
+
+BaseCube& BaseCube::executeInversionOfMove(const Move move) {
+    static const std::unordered_map<Move, BaseCube& (BaseCube::*)()> mp = {
+        {Move::L_PRIME, &BaseCube::l}, {Move::L, &BaseCube::lPrime},
+        {Move::R_PRIME, &BaseCube::r}, {Move::R, &BaseCube::rPrime},
+        {Move::U_PRIME, &BaseCube::u}, {Move::U, &BaseCube::uPrime},
+        {Move::D_PRIME, &BaseCube::d}, {Move::D, &BaseCube::dPrime},
+        {Move::F_PRIME, &BaseCube::f}, {Move::F, &BaseCube::fPrime},
+        {Move::B_PRIME, &BaseCube::b}, {Move::B, &BaseCube::bPrime}
+    };
+    const auto it = mp.find(move);
+    return (this->*it->second)();
 }
